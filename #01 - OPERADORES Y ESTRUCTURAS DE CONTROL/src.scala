99ed658efe30e64e16ca1514fff829a13083ed4c
J. Nicolas Riano T <rianojnicolas@gmail.com>
2024-01-16 23:58:13 -0500

    Actualizando ejercicio #02, Operadores Aritmeticos

--- #01 - OPERADORES Y ESTRUCTURAS DE CONTROL/src.scala
+++ #01 - OPERADORES Y ESTRUCTURAS DE CONTROL/src.scala
@@ -1,4 +1,21 @@
 // Operadores Aritmeticos
+val a = 1
+val b = -2
+println("OPERADORES ARITMETICOS")
+val suma: Int  = a + b 
+println("La suma entre " + a + " y " + b + " es igual a " + suma)
+val resta: Int = a - b
+println("La resta entre " + a + " y " + b + " es igual a " + resta)
+val division: Float = a/b
+println(s"La division entre $a y $b es igual a $division")
+val producto: Int = a*b
+println(s"La multiplicacion entre $a y $b es igual a $producto")
+val potencia = a**b
+println(s"El numero $a elevado a la potencia $b es igual a $potencia")
+val modulo = a%b
+println(s"El resto de la division entre $a y $b es igual a $modulo")
+println("")
+
 
 // Operadores Relacionales
 
