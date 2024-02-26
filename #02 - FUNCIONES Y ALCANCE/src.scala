9cb1349286802f5ccfaa523af294e09d8ddd0783
J. Nicolas Riano T <rianojnicolas@gmail.com>
2024-02-25 20:03:00 -0500

    Actualizando Reto #02, Agregando funcion sin parametro ni retorno

--- #02 - FUNCIONES Y ALCANCE/src.scala
+++ #02 - FUNCIONES Y ALCANCE/src.scala
@@ -1,7 +1,24 @@
 object Main {
     def main(args: Array[String]): Unit = {
         // Variable Global
-        var varGlobal = "Muhos Exitos, y que tenas un buen día"
+        var varGlobal = "Muchos Exitos, y que tenas un buen día"
+
+        // Funciones:
+        // Funcion sin parametros ni retorno
+        def welcomePlayer(): Unit = {
+            println("""
+            Hola People !!, este es un juego improvisado de
+            piedra, papel o tijera.
         
+            las reglas son las siguientes:
+            """)
+        }
+
+
+        def run(): Unit = {
+            welcomePlayer()
+        }
+        
+        run()
     }
 }
