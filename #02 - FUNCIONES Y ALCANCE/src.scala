9e4d9747bbb777646f7ef71e56800ae8326b44d4
J. Nicolas Riano T <rianojnicolas@gmail.com>
2024-02-26 20:59:28 -0500

    Actualizando Reto #02, Agregando funcion dentro de otra funcion

--- #02 - FUNCIONES Y ALCANCE/src.scala
+++ #02 - FUNCIONES Y ALCANCE/src.scala
@@ -7,11 +7,24 @@ object Main {
         // Funcion sin parametros ni retorno
         def welcomePlayer(): Unit = {
             println("""
-            Hola People !!, este es un juego improvisado de
-            piedra, papel o tijera.
+        Hola People !!, este es un juego improvisado de
+        piedra, papel o tijera.
         
-            las reglas son las siguientes:
+        las reglas son las siguientes:
+        """)
+        // Funcion dentro de otra funcion
+            def reglas() = {
+                print("""
+            1. Debes ingresar el objeto con el que jugaras
+            2. El pc elegira aleatoriamente un objeto
+            3. Recuerda que: papel le gana a piedra,
+                            piedra le gana a tijera,
+                            tijera le gana a papel.
+            
+            Muchos Exitooooss !!!
             """)
+            }
+            reglas()
         }
 
 
