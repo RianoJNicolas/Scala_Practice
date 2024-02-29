21fe0dffa2eea54bd0d3726d849d727db8c266b0
J. Nicolas Riano T <rianojnicolas@gmail.com>
2024-02-28 22:15:35 -0500

    Actualizando Reto #03, Agregando Lista

--- #03 - ESTRUCTURAS DE DATOS/src.scala
+++ #03 - ESTRUCTURAS DE DATOS/src.scala
@@ -1,3 +1,10 @@
+object Main {
+    def main(args: Array[String]): Unit = {
 
-// 1. Estrucutras de datos
+        // 1. Estrucutras de datos
 
+        // 1.1 Listas:
+        val myList = List(1, 2, 3, 4, 5)
+        println(s"Lista = $myList\n")
+    }
+}
