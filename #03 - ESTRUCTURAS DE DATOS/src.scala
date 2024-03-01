007e4fd31b00bd2667c5784d0405d8d4f87c986c
J. Nicolas Riano T <rianojnicolas@gmail.com>
2024-02-29 22:20:49 -0500

    Actualizando Reto #03, Agregando secuencias

--- #03 - ESTRUCTURAS DE DATOS/src.scala
+++ #03 - ESTRUCTURAS DE DATOS/src.scala
@@ -6,5 +6,9 @@ object Main {
         // 1.1 Listas:
         val myList = List(1, 2, 3, 4, 5)
         println(s"Lista = $myList\n")
+
+        // 1.2 Secuencias:
+        val mySeq = Seq(1, 2, 3, 4, 5)
+        println(s"Secuencia = $mySeq\n")
     }
 }
