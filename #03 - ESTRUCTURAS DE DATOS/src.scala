e973801e24524a8bbf26430ece0a81c8721876c7
J. Nicolas Riano T <rianojnicolas@gmail.com>
2024-03-03 22:02:20 -0500

    Actualizando Reto #03, Agregando array y vector

--- #03 - ESTRUCTURAS DE DATOS/src.scala
+++ #03 - ESTRUCTURAS DE DATOS/src.scala
@@ -18,5 +18,13 @@ object Main {
         // 1.3 Tuplas:
         val myTupla = (1, "one", 7.0, "seven")
         println(s"Tupla = $myTupla\n")
+
+        // 1.4 Array:
+        val myArray = Array(1, 2, 3, 4, 5)
+        println(s"Array = ${myArray.toString}\n")
+
+        // 1.5 Vector:
+        val myVector = Vector(1, 2, 3, 4, 5)
+        println(s"Vector = $myVector\n")
     }
 }
