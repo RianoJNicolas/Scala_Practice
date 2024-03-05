33e48d12d132fc5bdff99fc71fe0a25fb77c6021
J. Nicolas Riano T <rianojnicolas@gmail.com>
2024-03-04 22:10:40 -0500

    Actualizando Reto #03, Agregando v1 operaciones con Listas

--- #03 - ESTRUCTURAS DE DATOS/src.scala
+++ #03 - ESTRUCTURAS DE DATOS/src.scala
@@ -30,5 +30,18 @@ object Main {
         // 1.6 Sets:
         val mySet = Set(1, 2, 3, 4, 5)
         println(s"Set = $mySet\n")
+
+        // 2. Operando con las estructuras
+
+        // Listas:
+        println(s"Lista Inicial = ${myList}")
+        myList.append(6) # Insercion
+        print(f'Lista despues de agregar un elemento = {myList}')
+        myList.pop(1) # Borrado
+        print(f'Lista luego de borrar un elemento = {myList}')
+        myList[0] = 100 # Actualizacion
+        print(f'Lista actualizada = {myList}')
+        myList.sort() # Ordenacion
+        print(f'Lista ordenada = {myList}\n')
     }
 }
