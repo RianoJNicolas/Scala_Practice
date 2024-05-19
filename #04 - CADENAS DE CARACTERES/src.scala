626431d5754950bf9217e84c004ef91b31d9b2f6
J. Nicolas Riano T <rianojnicolas@gmail.com>
2024-05-19 00:11:31 -0500

    asignacion, concatenacion y aritmeticas con strings terminadas

--- #04 - CADENAS DE CARACTERES/src.scala
+++ #04 - CADENAS DE CARACTERES/src.scala
@@ -7,7 +7,13 @@ object Main {
         val nombre = "Mr X"
 
         // 2. Concatenacion
-        val final_saludo = saludo + nombre
-        println(s"${final_saludo}") 
+        val final_saludo_1 = saludo + nombre
+        println(s"${final_saludo_1}\n")
+        val final_saludo_2 = saludo.concat(nombre)
+        println(s"${final_saludo_2}\n")
+
+        // 3. Operaciones "Aritmeticas"
+        val saludoTotal = saludo*3
+        println(s"${saludoTotal}\n")
     }
 }
