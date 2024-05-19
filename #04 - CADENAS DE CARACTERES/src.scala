a2845a2418931f778a639e43ad068ac7e5513b0d
J. Nicolas Riano T <rianojnicolas@gmail.com>
2024-05-18 23:59:23 -0500

    Primer ejercicio

--- #04 - CADENAS DE CARACTERES/src.scala
+++ #04 - CADENAS DE CARACTERES/src.scala
@@ -1,5 +1,13 @@
 object Main {
     def main(args: Array[String]): Unit = {
-        
+        // OPERACIONES CON CADENAS DE CARACTERES
+
+        // 1. Asignacion
+        val saludo = "Hola, "
+        val nombre = "Mr X"
+
+        // 2. Concatenacion
+        val final_saludo = saludo + nombre
+        println(s"${final_saludo}") 
     }
 }
