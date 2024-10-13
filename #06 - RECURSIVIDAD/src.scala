d70bc71de0ee74fafe9398bafe3ba122b8046db6
J. Nicolas Riano T <rianojnicolas@gmail.com>
2024-10-13 01:12:46 -0500

    Adding First Excercise Difficult Extra in challenge #06

--- #06 - RECURSIVIDAD/src.scala
+++ #06 - RECURSIVIDAD/src.scala
@@ -13,20 +13,26 @@ object Source6 {
                 print_numbers(n-1)
             }
         }
-        print_numbers(100)
+        print_numbers(100) // Imprime los números del 100 al 0
+
+        // DIFICULTAD EXTRA
+        // 1. FUNCION QUE CALCULE EL FACTORIAL DE UN NÚMERO
+        def calc_factorial(n:Int): Int = {
+            if (n == 0) {
+                return 1
+            }
+            else {
+                return n*calc_factorial(n-1)
+            }
+        }
+
+        println(calc_factorial(5))  // Imprime el factorial de 5 = 120
+
+    
     }
 }
 
 /*  
-
-# EJERCICIO:
-# función recursiva que imprima números del 100 al 0.
-def print_numbers(n):
-    if n >= 0:
-        print(n)
-        print_numbers(n-1)
-
-print_numbers(100)  # Imprime los números del 100 al 0
 
 # DIFICULTAD EXTRA:
 # 1. Factorial de un numero n
