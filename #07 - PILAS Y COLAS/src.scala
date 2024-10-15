63116e2ccc9ef0b88c237d95d97aa335a6d41dbb
J. Nicolas Riano T <rianojnicolas@gmail.com>
2024-10-15 18:48:16 -0500

    update stackmethod challenge #07

--- #07 - PILAS Y COLAS/src.scala
+++ #07 - PILAS Y COLAS/src.scala
@@ -6,6 +6,19 @@ object Source7 {
     def main(args: Array[String]): Unit = {
         // EJERCICIO
 
+        // Listas como Pilas - LIFO (last in first out)
+        def stackMethod(stack: List[Int], action: String, element: Int): Unit = {
+            if (action == "push") {
+                var new_stack = stack :+ element    
+                return new_stack
+            }
+            else if (action == "pop") {
+                
+                return stack
+            }
+            elif action == "peek":
+                return stack[len(stack)-1]
+        }
         
     }
 }
