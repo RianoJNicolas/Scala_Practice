52f507450eff222097fb246a77db64cf1197b2b2
J. Nicolas Riano T <rianojnicolas@gmail.com>
2024-10-16 19:09:14 -0500

    update stackmethod-action peek challenge #07

--- #07 - PILAS Y COLAS/src.scala
+++ #07 - PILAS Y COLAS/src.scala
@@ -7,26 +7,27 @@ object Source7 {
         // EJERCICIO
 
         // Listas como Pilas - LIFO (last in first out)
-        def stackMethod(stack: List[Int], action: String, element: Int): (List[Int], Option[Int]) = {
+        def stackMethod(stack: List[Int], action: String, element: Int): (List[Int], Int) = {
             action match {
                 case "push" => 
                     var new_stack = stack :+ element
-                    (new_stack, Some(element))
-                }
-            /*else if (action == "pop") {
-                var new_stack = stack.tail
-                return (new_stack, stack.head)
-            }
-            else if (action == "peek") {
-                return (stack, stack.head)
-            }*/
+                    (new_stack, element)
                 
+                case "pop" => 
+                    var new_stack = stack.tail
+                    (new_stack, stack.head)
+                
+                case "peek" =>
+                    (stack, stack.head)
+                }    
         }
 
         var Mystack = List(1,2,3,4,5)
-        println(stackMethod(Mystack, "push", 6)(0))
-        //println(stackMethod(Mystack, "pop", 0))
-        //println(stackMethod(Mystack, "peek", 0))
+        var Mystack_push = stackMethod(Mystack, "push", 6)(0)
+        println(Mystack_push)
+        var Mystack_pop = stackMethod(Mystack_push, "pop", 0)(0)
+        println(Mystack_pop)
+        println(stackMethod(Mystack_pop, "peek", 0)(1))
 
         
     }
