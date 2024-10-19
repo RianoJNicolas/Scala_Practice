bda48e8158370fce9985f121b3e9afecbdb349c2
J. Nicolas Riano T <rianojnicolas@gmail.com>
2024-10-18 22:23:35 -0500

    Fix colas method and remove stack method in challenge #07

--- #07 - PILAS Y COLAS/src.scala
+++ #07 - PILAS Y COLAS/src.scala
@@ -6,7 +6,39 @@ object Source7 {
     def main(args: Array[String]): Unit = {
         // EJERCICIO
 
-        // Listas como Pilas - LIFO (last in first out)
+        // Listas como Colas - FIFO (first in first out)
+        def queueMethod(stack: List[Int], action: String, element: Int): (List[Int], Int) = {
+            action match {
+                case "enqueue" => 
+                    var new_stack = stack :+ element
+                    (new_stack, element)
+                
+                case "dequeue" => 
+                    var new_stack = stack.tail
+                    (new_stack, stack.head)
+                
+                case "peek" =>
+                    (stack, stack.head)
+                }    
+        }
+
+        println("FIFO - Colas (Queues)")
+        var Mystack = List(1,2,3,4,5)
+        var Mystack_push = queueMethod(Mystack, "enqueue", 6)(0)
+        println(Mystack_push)
+        var Mystack_pop = queueMethod(Mystack_push, "dequeue", 0)(0)
+        println(Mystack_pop)
+        println(queueMethod(Mystack_pop, "peek", 0)(1))
+
+        // Listas como Colas - FIFO (first in first out)
+
+        /* def queueMethod(queue: List[Int], action: String, element: Int): (List[Int], Int) = {
+            action match {
+                case "enqueue" =>
+                    var new_queue = element +: queue
+                    (new_queue, element)
+                
+                case "dequeue" =>
         def stackMethod(stack: List[Int], action: String, element: Int): (List[Int], Int) = {
             action match {
                 case "push" => 
@@ -22,17 +54,6 @@ object Source7 {
                 }    
         }
 
-        var Mystack = List(1,2,3,4,5)
-        var Mystack_push = stackMethod(Mystack, "push", 6)(0)
-        println(Mystack_push)
-        var Mystack_pop = stackMethod(Mystack_push, "pop", 0)(0)
-        println(Mystack_pop)
-        println(stackMethod(Mystack_pop, "peek", 0)(1))
-
-        // Listas como Colas - FIFO (first in first out)
-
-        def queueMethod(queue: List[Int], action: String, element: Int): (List[Int], Int) = {
-            action match {
-        }
+        }*/
     }
 }
